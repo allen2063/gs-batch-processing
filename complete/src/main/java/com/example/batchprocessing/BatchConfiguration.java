bf862e40a00a7f8888b83d9e87e91151720d66a2
zengyifei <zengyifei@foudnersc.com>
2020-03-23 23:03:03 +0800

    添加数据源

--- complete/src/main/java/com/example/batchprocessing/BatchConfiguration.java
+++ complete/src/main/java/com/example/batchprocessing/BatchConfiguration.java
@@ -11,7 +11,11 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
@@ -22,6 +26,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import javax.persistence.EntityManagerFactory;
+
 
 // tag::setup[]
 @Configuration
@@ -33,6 +39,13 @@ public class BatchConfiguration {
 
 	@Autowired
 	public StepBuilderFactory stepBuilderFactory;
+
+//	@Autowired
+//	private final EntityManagerFactory managerFactory;
+
+	private DataSource dataSource;
+
+
 	// end::setup[]
 
 	// tag::readerwriterprocessor[]
@@ -47,6 +60,26 @@ public class BatchConfiguration {
 				setTargetType(Person.class);
 			}})
 			.build();
+	}
+
+//	@Bean
+//	public JpaPagingItemReader<Person> jpaReader(){
+//		return new JpaPagingItemReaderBuilder<Person>()
+//				.entityManagerFactory(managerFactory)
+//				.queryString("select i.msg_id,i.msg_content,j.obj_mobile from jqy.jqy_t_msg_inf i\n" +
+//						"left join jqy.jqy_t_msg_sendobj j\n" +
+//						"on i.msg_id= j.msg_id")
+//				.name("msgSendReaders")
+//				.build();
+//	}
+
+	@Bean
+	public JdbcPagingItemReader<Person> jdbcReaders(){
+		return new JdbcPagingItemReaderBuilder<Person>()
+				.dataSource(dataSource)
+				.selectClause("elect i.msg_id,i.msg_content")
+				.fromClause("jqy_t_msg_inf")
+				.build();
 	}
 
 	@Bean
@@ -79,7 +112,7 @@ public class BatchConfiguration {
 	public Step step1(JdbcBatchItemWriter<Person> writer) {
 		return stepBuilderFactory.get("step1")
 			.<Person, Person> chunk(10)
-			.reader(reader())
+			.reader(jdbcReaders())
 			.processor(processor())
 			.writer(writer)
 			.build();
---
+++ complete/src/main/java/com/example/batchprocessing/JqyMsg.java
@@ -0,0 +1,11 @@
+package com.example.batchprocessing;
+
+
+/**
+ * Created by ZengYiFei on 2020/3/23.
+ */
+public class JqyMsg {
+    private String msgId;
+    private String msgContent;
+    private String msgSendObj;
+}
